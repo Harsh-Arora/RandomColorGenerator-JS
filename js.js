e72e71374c84b078e6ca4c9651e3472ef31b08de
Harsh Arora <harsh.arora1821@gmail.com>
2019-12-06 09:36:08 +0800

    Copy function added

--- js.js
+++ js.js
@@ -50,3 +50,13 @@ document.getElementById('res4').style.background = document.getElementById('res4
 
 document.getElementById('res5code').innerHTML = generator();
 document.getElementById('res5').style.background = document.getElementById('res5code').innerHTML;
+
+
+function ctc(elementID){
+	var aux = document.createElement("input");
+	aux.setAttribute('Value',document.getElementById(elementID).innerHTML);
+ document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+}
